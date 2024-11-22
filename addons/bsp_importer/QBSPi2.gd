c12f115958dfdfc0a43400ab7b346e5b55e084ca
CSLRDoesntGameDev <126827743+CSLRDoesntGameDev@users.noreply.github.com>
2024-11-22 14:51:54 -0700

    Refactored Directory To Work Properly, Geometry Seems to Work Fine Now.

--- addons/bsp_importer/QBSPi2.gd
+++ addons/bsp_importer/QBSPi2.gd
@@ -1,9 +1,35 @@
 @tool class_name BSPi2 extends Node3D
 
 
-## QBSPi2 (Quake BSP Importer 2) By CSLR.
-## As of November 21 2024 Converts Quake 2 Geometry To Edges.
+## QBSPi2 (Quake BSPi Importer 2) By CSLR.
 
+
+## based on documentation from:
+## https://jheriko-rtw.blogspot.com/2010/11/dissecting-quake-2-bsp-format.html
+## https://www.flipcode.com/archives/Quake_2_BSP_File_Format.shtml
+##
+## I Hope This Isn't Gonna End Up GPL'd, If It Does I'm fine releasing it myself under GPL 
+
+enum {LUMP_OFFSET, LUMP_LENGTH}
+
+enum {
+	LUMP_ENT,
+	LUMP_PLANE,
+	LUMP_VERTEX,
+	LUMP_VIS,
+	LUMP_NODE,
+	LUMP_TEXTURE,
+	LUMP_FACE,
+	LUMP_LIGHTMAP,
+	LUMP_LEAVES,
+	LUMP_LEAFFACETABLE,
+	LUMP_LEAFBRUSHTABLE,
+	LUMP_EDGE,
+	LUMP_FACE_EDGE,
+	LUMP_MODEL,
+	LUMP_BRUSH,
+	LUMP_BRUSHSIDE
+}
 
 @export var bsp_file : String
 
@@ -12,6 +38,10 @@
 @export var MeshPrim = Mesh.PRIMITIVE_LINES : set = setmp; func setmp(m): MeshPrim = m
 
 @export var meshinstance : MeshInstance3D : set = setmi; func setmi(m): meshinstance = m
+
+
+
+var geometry := {}
 
 func set_convertBSP(c): 
 	convertBSP = c
@@ -40,68 +70,82 @@ func convert_to_mesh():
 	
 	await get_tree().create_timer(0.1).timeout
 	
-	var bsp_version = convert_from_uint32(bytes(bsp_bytes, range(4, 8)))
+	var bsp_version = str(convert_from_uint32(bytes(bsp_bytes, range(4, 8))))
+	var magic_num = bytes(bsp_bytes, range(0, 4)).get_string_from_utf8()
 	
-	prints("QBSPi2 Found BSP Version %s, Expecting Version 38 for Quake 2 BSP." % bsp_version)
+	prints("QBSPi2 Found BSP Version %s %s, Expecting Version IBSP 38, Version 46 May Not Be Supported Yet." % [magic_num, bsp_version])
 	
 	var directory = fetch_directory(bsp_bytes)
 	
-	var vkey = directory.keys()[2] # vertices
 	
-	var fkey = directory.keys()[6] # faces
+	# i regret this code!
+	var vertex_lmp = range(directory[LUMP_VERTEX].get(LUMP_OFFSET), directory[LUMP_VERTEX].get(LUMP_OFFSET)+directory[LUMP_VERTEX].get(LUMP_LENGTH))
+	var face_edge_lmp = range(directory[LUMP_FACE_EDGE].get(LUMP_OFFSET), directory[LUMP_FACE_EDGE].get(LUMP_OFFSET)+directory[LUMP_FACE_EDGE].get(LUMP_LENGTH))
+	var face_lmp = range(directory[LUMP_FACE].get(LUMP_OFFSET), directory[LUMP_FACE].get(LUMP_OFFSET)+directory[LUMP_FACE].get(LUMP_LENGTH))
+	var edge_lmp = range(directory[LUMP_EDGE].get(LUMP_OFFSET), directory[LUMP_EDGE].get(LUMP_OFFSET)+directory[LUMP_EDGE].get(LUMP_LENGTH))
 	
-	var ekey = directory.keys()[11] # edges
-	var fekey = directory.keys()[12]  # face edges
+	#print(vertex_dir)
 	
-	var vert_bytes = bytes(bsp_bytes, range(vkey, vkey+directory[vkey]))
-	var face_bytes = bytes(bsp_bytes, range(fkey, fkey+directory[fkey]))
+	geometry["vertex"] = get_verts(bytes(bsp_bytes, vertex_lmp))
+	geometry["face"] = get_face_lump(bytes(bsp_bytes, face_lmp))
 	
-	var edge_bytes = bytes(bsp_bytes, range(ekey, ekey+directory[ekey]))
-	var face_edge_bytes = bytes(bsp_bytes, range(fekey, fekey+directory[fekey]))
+	geometry["edge"] = get_edges(bytes(bsp_bytes, edge_lmp))
+	geometry["face_edge"] = get_face_edges(bytes(bsp_bytes, face_edge_lmp))
 	
-	var verts = get_verts(vert_bytes)
-	var faces : Array[BSPFace] = get_face_lump(face_bytes)
-	var edges = get_edges(edge_bytes)
-	var face_edges = get_face_edges(face_edge_bytes)
+	print(geometry)
 	
-	#for face in faces: prints(face.plane, face.plane_side, face.first_edge, face.num_edges, face.lightmap_styles, face.lightmap_offset) # for debugging
+	var geometry = process_to_mesh_array(geometry)
 	
-	var vertex_edge_array = process_to_edge_array(verts, faces, edges, face_edges)
-	if meshinstance: meshinstance.mesh = create_mesh(vertex_edge_array, edges)
+	if meshinstance: meshinstance.mesh = create_mesh(geometry)
 	
 
+
 ## takes the vertex, face, edge and face edge arrays and outputs an array of all the edge points.
-func process_to_edge_array(verts : PackedVector3Array, face_array : Array, edge_array : Array, face_edge_array : Array) -> PackedVector3Array:
+func process_to_mesh_array(geometry : Dictionary) -> PackedVector3Array:
 	var output_verts : PackedVector3Array = []
+	var face_vertex_indices = []
 	
-	for face in face_array:
-		face = face as BSPFace
-		var face_edges = range(face.first_edge, face.first_edge+face.num_edges)
+	var verts = geometry.get("vertex")
+	var edges = geometry.get("edge")
+	var face_edges = geometry.get("face_edge")
+	for face_index in geometry.get("face"):
+		var face = face_index as BSPFace
+		var first_edge = face.first_edge
+		var num_edges = face.num_edges
 		
-		for face_edge in face_edges:
-			if face_edge < edge_array.size():
-				var edge = edge_array[abs(face_edge)]
-				
-				var edge0 = edge[0]
-				var edge1 = edge[1]
-				
-				if face_edge > 0: 
-					edge0 = edge[0]
-					edge1 = edge[1]
-				
-				if face_edge < 0: 
-					edge0 = edge[1]
-					edge1 = edge[0]
-				
-				# divide to conversion from quake units to 1 meter
-				
-				var vert0 = Vector3(verts[edge0])
-				var vert1 = Vector3(verts[edge1])
-				
-				
-				output_verts.append_array([vert0, vert1])
+		var edge_range = range(first_edge, first_edge + num_edges)
+		
+		for edge in edge_range:
+			var face_edge = face_edges[edge]
+			
+			edge = edges[abs(face_edge)]
+			
+			var edge0 = edge[0]
+			var edge1 = edge[1]
+			
+			if face_edge > 0:
+				edge0 = edge[0]
+				edge1 = edge[1]
+			
+			if face_edge < 0:
+				edge0 = edge[1]
+				edge1 = edge[0]
+ 			
+			
+			
+			var vert0 = verts[edge0]
+			var vert1 = verts[edge1]
+			
+			output_verts.append_array([vert0, vert1])
+			
+			
+		
+		
+		
 	
-	return output_verts 
+	
+	return output_verts
+
 
 
 ## grabs the directory
@@ -109,14 +153,16 @@ func fetch_directory(bsp_bytes):
 	prints("QBSPi2 BSP File Size:", bsp_bytes.size())
 	var i = 0
 	var dir = {}
-	var dir_lump = bytes( bsp_bytes, range(8, (19*12) ))
+	var dir_lump = bytes( bsp_bytes, range(8, ( 19 * 8 ) ))
 	
-	while i < dir_lump.size():
-		if i+4 <= dir_lump.size() and i+8 <= dir_lump.size():
-			var u32_key = convert_from_uint32(bytes(dir_lump, range(i, i+4)))
-			var u32_value = convert_from_uint32(bytes(dir_lump, range(i+4, i+8)))
-			dir[u32_key] = u32_value
-		i += 8
+	for lump in range(0, dir_lump.size()-8, 8):
+		
+		var offset = bytes(dir_lump, range(lump+0, lump+4)).decode_u32(0)
+		var length = bytes(dir_lump, range(lump+4, lump+8)).decode_u32(0)
+		
+		dir[lump / 8] = {LUMP_OFFSET: offset, LUMP_LENGTH: length}
+		
+		
 	return dir
 
 ## returns vertex lump
@@ -136,36 +182,31 @@ func get_verts(vert_bytes : PackedByteArray) -> PackedVector3Array:
 		vertex_array.append(vec)
 		v += 1
 	return vertex_array
-	
+
 ## returns face lump
 func get_face_lump(lump_bytes : PackedByteArray):
 	var count = lump_bytes.size() / 20
 	prints("QBSPi2 Calculated Estimate of %s Faces" % count)
 	var faces : Array[BSPFace] = []
 	var f = 0
-	
+
 	while f < count:
 		var new_face := BSPFace.new()
 		var base_index = f * 20
 		
-		new_face.plane = bytes(lump_bytes, range(base_index + 0, base_index + 2)).decode_u16(0)
-		new_face.plane_side = bytes(lump_bytes, range(base_index + 2, base_index + 4)).decode_u16(0)
+		var by = bytes(lump_bytes, range(base_index, base_index + 20))
 		
-		new_face.first_edge = bytes(lump_bytes, range(base_index + 4, base_index + 8)).decode_u32(0)
-		new_face.num_edges = bytes(lump_bytes, range(base_index + 8, base_index + 10)).decode_u16(0)
+		new_face.plane      = by.decode_u16(0)
+		new_face.plane_side = by.decode_u16(2)
+		new_face.first_edge = by.decode_u32(4)
+		new_face.num_edges  = by.decode_u16(8)
 		
-		new_face.texture_info = bytes(lump_bytes, range(base_index + 10, base_index + 12)).decode_u16(0)
 		
-		var lms = []
-		for i in [0, 1, 2, 3]: 
-			lms.append(bytes(lump_bytes, range(base_index + 12 + i, base_index + 13 + i)).decode_u8(0))
-		
-		new_face.lightmap_styles = lms
-		new_face.lightmap_offset = bytes(lump_bytes, range(base_index + 16, base_index + 20)).decode_u32(0)
+		prints(new_face.plane, new_face.plane_side, new_face.first_edge, new_face.num_edges)
 		
 		faces.append(new_face)
 		f += 1
-	
+
 	return faces
 
 ## returns edge lump
@@ -175,8 +216,9 @@ func get_edges(edge_bytes : PackedByteArray):
 	var edges = []
 	while e < count:
 		var index = e * 4
-		var edge_1 = bytes(edge_bytes, range(index + 0, index + 2)).decode_s16(0)
-		var edge_2 = bytes(edge_bytes, range(index + 2, index + 4)).decode_s16(0)
+		var edge_1 = bytes(edge_bytes, range(index + 0, index + 2)).decode_u16(0)
+		var edge_2 = bytes(edge_bytes, range(index + 2, index + 4)).decode_u16(0)
+		
 		
 		edges.append([edge_1, edge_2])
 		e += 1
@@ -195,16 +237,16 @@ func get_face_edges(face_bytes : PackedByteArray):
 		var f1 = bytes(face_bytes, range(index + 0, index + 4)).decode_s32(0)
 		face_edges.append(f1)
 		f += 1
-	
+
 	return face_edges
 
 ## creates and returns an ImmediateMesh, as of right now uses the edges but should use tris.
-func create_mesh(verts : PackedVector3Array, edges : Array) -> Mesh:
+func create_mesh(verts : PackedVector3Array) -> Mesh:
 	var mesh = ImmediateMesh.new()
 	mesh.surface_begin(MeshPrim)
 	for vert in verts: mesh.surface_add_vertex(vert)
 	mesh.surface_end()
-	
+
 	return mesh
 
 ## converts 2 bytes to a unsigned int16
@@ -212,13 +254,13 @@ func convert_from_uint16(uint16):
 	var uint16_value = uint16.decode_u16(0)
 	if uint16.size() < 4: return 0
 	return uint16_value
-	
+
 ## converts 4 bytes to a unsigned int32
 func convert_from_uint32(uint32 : PackedByteArray):
 	var uint32_value = uint32.decode_u32(0)
 	if uint32.size() < 4: return 0
 	return uint32_value
-	
+
 ## returns bytes, indices should be an array e.g. [1, 2, 3, 4]
 func bytes(input_array, indices : Array) -> PackedByteArray:
 	var output_array = []
