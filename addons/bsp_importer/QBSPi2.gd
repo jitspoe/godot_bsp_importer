f07e5deac71862c5703369ba740a7d4a105c4bf7
CSLRDoesntGameDev <126827743+CSLRDoesntGameDev@users.noreply.github.com>
2024-11-22 20:59:55 -0700

    Added Texturing / Refactored Mesh Construction Moving Face Verts to Stored in the BSPFace Class.
    There is a Minor UV Offset on Top/Bottom Faces, I Cannot Be Bothered To Worry About This right now.

--- addons/bsp_importer/QBSPi2.gd
+++ addons/bsp_importer/QBSPi2.gd
@@ -32,6 +32,8 @@ enum {
 }
 
 @export var bsp_file : String
+@export var textures_path : String = "res://Assets/textures/"
+@export var texture_extension : String = ".jpg"
 
 @export var convertBSP : bool = false : set = set_convertBSP; 
 
@@ -40,8 +42,8 @@ enum {
 @export var meshinstance : MeshInstance3D : set = setmi; func setmi(m): meshinstance = m
 
 
-
 var geometry := {}
+var textures := {}
 
 func set_convertBSP(c): 
 	convertBSP = c
@@ -59,10 +61,30 @@ class BSPFace:
 	var first_edge : int = 0 # uint32
 	var num_edges : int = 0 # uint16
 	
-	var texture_info : int = 0 # uint16
+	var texture_index : int = 0 # uint16
 	
 	var lightmap_styles : Array = [null, null, null, null] # uint8 array
 	var lightmap_offset : int = 0 # uint32
+	
+	var verts : PackedVector3Array = [] # For Mesh Construction
+
+## Houses Texture Info
+class BSPTexture extends Node:
+	
+	var u_axis : Vector3
+	var u_offset : float 
+	
+	var v_axis : Vector3
+	var v_offset : float 
+	
+	var flags : int # uint32
+	var value : int # uint32
+	
+	var texture_path : String # uint32
+	
+	var next_textinfo : int # uint32
+	
+	
 
 ## Central Function
 func convert_to_mesh():
@@ -77,14 +99,16 @@ func convert_to_mesh():
 	
 	var directory = fetch_directory(bsp_bytes)
 	
-	
 	# i regret this code!
 	var vertex_lmp = range(directory[LUMP_VERTEX].get(LUMP_OFFSET), directory[LUMP_VERTEX].get(LUMP_OFFSET)+directory[LUMP_VERTEX].get(LUMP_LENGTH))
 	var face_edge_lmp = range(directory[LUMP_FACE_EDGE].get(LUMP_OFFSET), directory[LUMP_FACE_EDGE].get(LUMP_OFFSET)+directory[LUMP_FACE_EDGE].get(LUMP_LENGTH))
 	var face_lmp = range(directory[LUMP_FACE].get(LUMP_OFFSET), directory[LUMP_FACE].get(LUMP_OFFSET)+directory[LUMP_FACE].get(LUMP_LENGTH))
 	var edge_lmp = range(directory[LUMP_EDGE].get(LUMP_OFFSET), directory[LUMP_EDGE].get(LUMP_OFFSET)+directory[LUMP_EDGE].get(LUMP_LENGTH))
 	
-	#print(vertex_dir)
+	var texture_lmp = range(directory[LUMP_TEXTURE].get(LUMP_OFFSET), directory[LUMP_TEXTURE].get(LUMP_OFFSET)+directory[LUMP_TEXTURE].get(LUMP_LENGTH))
+	
+	geometry = {}
+	textures = {}
 	
 	geometry["vertex"] = get_verts(bytes(bsp_bytes, vertex_lmp))
 	geometry["face"] = get_face_lump(bytes(bsp_bytes, face_lmp))
@@ -92,16 +116,17 @@ func convert_to_mesh():
 	geometry["edge"] = get_edges(bytes(bsp_bytes, edge_lmp))
 	geometry["face_edge"] = get_face_edges(bytes(bsp_bytes, face_edge_lmp))
 	
+	textures["lumps"] = get_texture_lmp(bytes(bsp_bytes, texture_lmp))
 	
-	var geometry = process_to_mesh_array(geometry)
+	process_to_mesh_array(geometry)
 	
-	if meshinstance: meshinstance.mesh = create_mesh(geometry)
+	if meshinstance: meshinstance.mesh = create_mesh(geometry["face"])
 	
 
 
 ## takes the vertex, face, edge and face edge arrays and outputs an array of all the edge points.
-func process_to_mesh_array(geometry : Dictionary) -> PackedVector3Array:
-	var output_verts : PackedVector3Array = []
+func process_to_mesh_array(geometry : Dictionary) -> void:
+	var output_verts = []
 	var face_vertex_indices = []
 	
 	var verts = geometry.get("vertex")
@@ -116,7 +141,7 @@ func process_to_mesh_array(geometry : Dictionary) -> PackedVector3Array:
 		var edge_range = range(first_edge, first_edge + num_edges)
 		
 		var face_vert_list = []
-		
+		var face_vert_out = []
 		
 		for edge in edge_range:
 			
@@ -142,36 +167,20 @@ func process_to_mesh_array(geometry : Dictionary) -> PackedVector3Array:
 			var vert0 = verts[edge0]
 			var vert1 = verts[edge1]
 			
-			if MeshPrim == Mesh.PRIMITIVE_LINES:
-				output_verts.append_array([vert0, vert1])
-			else:
-				face_vert_list.append_array([vert0, vert1])
-			
-		if MeshPrim == Mesh.PRIMITIVE_TRIANGLES:
-			
-			for v in range(0, face_vert_list.size()-2):
-				
-				var vert0 = face_vert_list[0]
-				var vert1 = face_vert_list[v + 1]
-				var vert2 = face_vert_list[v + 2]
-				
-				
-				output_verts.append_array([vert2, vert1, vert0])
-				
-				
-				
-			
-			
+			face_vert_list.append_array([vert0, vert1])
 			
 		
-		#print(face_edge_list)
+		for v in range(0, face_vert_list.size()-2):
+			
+			var vert0 = face_vert_list[0]
+			var vert1 = face_vert_list[v + 1]
+			var vert2 = face_vert_list[v + 2]
+			
+			face_vert_out.append(vert2)
+			face_vert_out.append(vert1)
+			face_vert_out.append(vert0)
 		
-		
-	
-	
-	
-	return output_verts
-
+		face.verts = face_vert_out
 
 
 ## grabs the directory
@@ -227,12 +236,59 @@ func get_face_lump(lump_bytes : PackedByteArray):
 		new_face.first_edge = by.decode_u32(4)
 		new_face.num_edges  = by.decode_u16(8)
 		
-		
+		new_face.texture_index = by.decode_u16(10)
 		
 		faces.append(new_face)
 		f += 1
-
+	
 	return faces
+
+## returns texture lump
+func get_texture_lmp(tex_bytes : PackedByteArray) -> Array:
+	var count = tex_bytes.size() / 76
+	prints("QBSPi2 Calculated Estimate of %s Texture References" % count)
+	
+	var output = []
+	
+	for b in range(0, tex_bytes.size(), 76):
+		var BSPTI := BSPTexture.new()
+		
+		var ux = bytes(tex_bytes, range(b, b + 4)).decode_float(0)
+		var uy = bytes(tex_bytes, range(b + 4, b + 8)).decode_float(0)
+		var uz = bytes(tex_bytes, range(b + 8, b + 12)).decode_float(0)
+		
+		var uoffset = bytes(tex_bytes, range(b + 12, b + 16)).decode_float(0)
+		
+		var vx = bytes(tex_bytes, range(b + 16, b + 20)).decode_float(0)
+		var vy = bytes(tex_bytes, range(b + 20, b + 24)).decode_float(0)
+		var vz = bytes(tex_bytes, range(b + 24, b + 28)).decode_float(0)
+		
+		var voffset = bytes(tex_bytes, range(b + 28, b + 32)).decode_float(0)
+		
+		var flags = bytes(tex_bytes, range(b + 32, b + 36)).decode_u32(0)
+		var value = bytes(tex_bytes, range(b + 36, b + 40)).decode_u32(0)
+		
+		var texture_path = bytes(tex_bytes, range(b + 40, b + 72)).get_string_from_utf8()
+		
+		var next_texinfo = bytes(tex_bytes, range(b + 72, b + 76)).decode_s32(0)
+		
+		BSPTI.u_axis = Vector3(ux, uy, uz)
+		BSPTI.u_offset = uoffset
+		BSPTI.v_axis = Vector3(vx, vy, vz)
+		BSPTI.v_offset = voffset
+		
+		BSPTI.flags = flags
+		BSPTI.value = value
+		
+		BSPTI.texture_path = texture_path
+		BSPTI.next_textinfo = next_texinfo
+		
+		BSPTI.name = str(BSPTI.texture_path)
+		
+		output.append(BSPTI)
+		
+	
+	return output
 
 ## returns edge lump
 func get_edges(edge_bytes : PackedByteArray):
@@ -266,27 +322,90 @@ func get_face_edges(face_bytes : PackedByteArray):
 	return face_edges
 
 ## creates and returns an ImmediateMesh, as of right now uses the edges but should use tris.
-func create_mesh(verts : PackedVector3Array) -> Mesh:
+func create_mesh(data : Array) -> Mesh:
 	var mesh = ImmediateMesh.new()
-	mesh.surface_begin(MeshPrim)
-	for vert in range(0, verts.size(), 3):
-		var a = verts[vert + 0]
-		var b = verts[vert + 1]
-		var c = verts[vert + 2]
-		 
-		var normal = (b - a).cross((c - a))
+	var texture_list = textures["lumps"]
+	var surface_list = {}
+	
+	if not textures_path.ends_with("/"): textures_path += "/"
+	
+	for texture in texture_list:
+		texture = texture as BSPTexture
+		if not surface_list.keys().has(texture):
+			surface_list[texture] = []
+	
+	for face in data:
+		face = face as BSPFace
+		var surface_entry = texture_list[face.texture_index]
+		surface_list[surface_entry].append(face)
+	
+	
+	var sfc = 0 # surface count
+	
+	
+	for surfaceTexture in surface_list:
+		var surface = surface_list[surfaceTexture]
+		if surface.size() > 0:
+			var texture = surfaceTexture as BSPTexture
+			mesh.surface_begin(MeshPrim)
+			var material = StandardMaterial3D.new()
+			if FileAccess.file_exists(textures_path + texture.texture_path + texture_extension):
+				var matTexture = load(textures_path + texture.texture_path + texture_extension)
+				material.albedo_texture = matTexture
+				prints("starting surface", sfc)
+				
+				
+				for face in surface:
+					face = face as BSPFace
+					var verts = face.verts as PackedVector3Array
+					for vertIndex in range(0, verts.size(), 3):
+						var v0 = verts[vertIndex + 0]
+						var v1 = verts[vertIndex + 1]
+						var v2 = verts[vertIndex + 2]
+						
+						var uv0 = get_uv(v0, texture.u_axis, texture.v_axis, texture.u_offset, texture.v_offset, matTexture.get_size())
+						var uv1 = get_uv(v1, texture.u_axis, texture.v_axis, texture.u_offset, texture.v_offset, matTexture.get_size())
+						var uv2 = get_uv(v2, texture.u_axis, texture.v_axis, texture.u_offset, texture.v_offset, matTexture.get_size())
+						
+						var normal = (v1 - v0).cross((v2 - v0))
+						
+						mesh.surface_set_normal(normal.normalized())
+						
+						mesh.surface_set_uv(uv0)
+						mesh.surface_add_vertex(v0)
+						
+						mesh.surface_set_uv(uv1)
+						mesh.surface_add_vertex(v1)
+						
+						mesh.surface_set_uv(uv2)
+						mesh.surface_add_vertex(v2)
+						
+				
+				
+				
+				mesh.surface_end()
+				mesh.surface_set_material(sfc, material)
+				sfc += 1
+				
+				print("finished surface")
 		
-		mesh.surface_set_color(Color(randf(), randf(), randf()))
-		
-		mesh.surface_set_normal(normal)
-		
-		mesh.surface_add_vertex(a)
-		mesh.surface_add_vertex(b)
-		mesh.surface_add_vertex(c)
-		
-	mesh.surface_end()
-
+	
+	
+	
+	
 	return mesh
+
+func get_uv(vertex : Vector3, u_axis : Vector3, v_axis : Vector3, u_offset : float, v_offset : float, tex_size : Vector2) -> Vector2:
+	u_offset = 0
+	v_offset = 0
+	var x = vertex.x / tex_size.x
+	var y = vertex.z / tex_size.y
+	var z = vertex.y / tex_size.y
+	
+	var u = x * u_axis.x + y * u_axis.y + z * u_axis.z + u_offset
+	var v = x * v_axis.x + y * v_axis.y + z * v_axis.z + v_offset
+	return Vector2(u, v)
+
 
 ## converts 2 bytes to a unsigned int16
 func convert_from_uint16(uint16):
